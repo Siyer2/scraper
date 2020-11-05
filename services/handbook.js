bd06494f376fb5d00b0f4d73f77153306712c795
Syam Iyer <iyersyam@gmail.com>
2020-11-05 22:28:39 +1100

    Removed unnecessary print

--- services/handbook.js
+++ services/handbook.js
@@ -299,7 +299,6 @@ function getCoursesFromRule(db, rule, programInfo) {
 
             axios(config)
                 .then(function (response) {
-                    console.log(JSON.stringify(response.data));
                     resolve(response.data);
                 })
                 .catch(function (error) {
